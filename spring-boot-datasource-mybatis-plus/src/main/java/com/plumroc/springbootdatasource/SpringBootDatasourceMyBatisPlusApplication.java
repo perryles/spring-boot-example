383a7147c381a31914325b51f96eacc03739e941
plumroc <55521402@qq.com>
2022-11-02 11:26:18 +0800

    initialization

---
+++ spring-boot-datasource-mybatis-plus/src/main/java/com/plumroc/springbootdatasource/SpringBootDatasourceMyBatisPlusApplication.java
@@ -0,0 +1,13 @@
+package com.plumroc.springbootdatasource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDatasourceMyBatisPlusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDatasourceMyBatisPlusApplication.class, args);
+    }
+
+}
---
+++ spring-boot-datasource-mybatis-plus/src/test/java/com/plumroc/springbootdatasource/SpringBootDatasourceMyBatisPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plumroc.springbootdatasource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDatasourceMyBatisPlusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
